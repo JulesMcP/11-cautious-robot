dd8bbca0e248abedbb0abfdcab4d8ace7927dba5
JulesMcP <rogue427@yahoo.com>
2022-05-14 11:26:15 -0500

    adds package for unique ID & api routes code

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const notes = require('../../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+// GET request for notes from db.json
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+// POST request to notes at db.json 
+router.post('/notes', (req, res) => {
+    // set random ID
+    req.body.id = uuidv4();
+    notes.push(req.body);
+    res.sendFile(__dirname + notes);
+    res.json(req.body);
+});
+
+module.exports = router;
