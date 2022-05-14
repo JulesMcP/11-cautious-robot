1ed16858b1a7bfde24ce0f15b2edbf29d190f23f
JulesMcP <rogue427@yahoo.com>
2022-05-14 11:30:02 -0500

    adds to htmlRoutes

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,19 @@
+// required components
+const path = require('path');
+const router = require('express').Router();
+
+// routes
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+// export router
+module.exports = router;
